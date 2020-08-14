c78d87da42c383cd7b05e1ef807301b24a6627dd
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2020-08-14 14:20:32 +0500

    test_deploy

---
+++ config.py
@@ -0,0 +1,37 @@
+import os
+from distutils.util import strtobool
+
+import dotenv
+
+dotenv.load_dotenv()
+
+
+class Config:
+    TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+    USE_POLLING = bool(strtobool(os.getenv("USE_POLLING", "False")))
+    PUBLIC_KEY = os.getenv("PUBLIC_KEY", None)
+
+    HOST = os.getenv("HOST")
+    WEBHOOK_PATH = "/webhook/" + TELEGRAM_TOKEN
+    WEBHOOK_URL = f"{HOST}{WEBHOOK_PATH}"
+
+    WEBAPP_HOST = "0.0.0.0"
+    WEBAPP_PORT = os.getenv("WEBAPP_PORT", 8000)
+    WEBHOOK_PORT = os.getenv("WEBHOOK_PORT", 8001)
+
+    # DATABASE_HOST = os.getenv("DATABASE_HOST")
+    # DATABASE_PORT = os.getenv("DATABASE_PORT")
+    # DATABASE_USERNAME = os.getenv("DATABASE_USERNAME")
+    # DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+    # DATABASE_NAME = os.getenv("DATABASE_NAME")
+    # DATABASE_ECHO = bool(strtobool(os.getenv("DATABASE_ECHO", "False")))
+
+    REDIS_HOST = os.getenv("REDIS_HOST")
+    REDIS_PORT = os.getenv("REDIS_PORT")
+    REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
+
+    # FLASK_DEBUG = os.getenv("FLASK_DEBUG", 0)
+    # SECRET_KEY = os.getenv("SECRET_KEY")
+
+    # ADMIN_LOGIN = os.getenv("ADMIN_LOGIN", "admin")
+    # ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "admin")
---
+++ project/__main__.py
@@ -0,0 +1,48 @@
+import logging
+
+from aiogram import executor
+# from aiogram.types import InputFile
+# from aiogram.utils.executor import start_webhook
+
+from config import Config
+# from tbot import handlers
+from project.bot import bot, dp
+
+logging.basicConfig(level=logging.INFO)
+
+
+# async def on_startup(app):
+#     logging.info(f"Setting up webhook url: {Config.WEBHOOK_URL}")
+#     await bot.delete_webhook()
+#     certificate = InputFile(Config.PUBLIC_KEY)
+#     status = await bot.set_webhook(Config.WEBHOOK_URL, certificate=certificate)
+#     if status:
+#         logging.info("Webhook setting up successfully!")
+#     else:
+#         logging.error("Failed to setting up webhook!")
+
+
+# async def on_shutdown(dp):
+#     logging.warning('Shutting down..')
+#     await bot.delete_webhook()
+
+#     await dp.storage.close()
+#     await dp.storage.wait_closed()
+#     logging.warning('Bye!')
+
+
+if __name__ == '__main__':
+    # handlers.user.setup(dp)
+    if Config.USE_POLLING:
+        executor.start_polling(dp, skip_updates=True)
+    else:
+        pass
+        # start_webhook(
+        #     dispatcher=dp,
+        #     webhook_path=Config.WEBHOOK_PATH,
+        #     on_startup=on_startup,
+        #     on_shutdown=on_shutdown,
+        #     skip_updates=True,
+        #     host=Config.WEBAPP_HOST,
+        #     port=Config.WEBHOOK_PORT,
+        # )
