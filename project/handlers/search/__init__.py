868b3b6d7a7107ec027a6ba3e01285853ba26943
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2020-08-15 18:11:46 +0500

    fix button ordering and add some

--- project/handlers/search/__init__.py
+++ project/handlers/search/__init__.py
@@ -9,6 +9,7 @@ from .keyboard import (Search, back_to_search, back_to_search_again, handle_inva
 
 def setup(dp: Dispatcher):
     dp.register_message_handler(handle_query, state=Search.wait_for_query)
+    dp.register_message_handler(back_to_search_again, lambda message: message.text == TRY_SEARCH_AGAIN, state=Search.wait_for_question)
     dp.register_message_handler(handle_question, state=Search.wait_for_question)
     dp.register_message_handler(back_to_search_again, lambda message: message.text == TRY_SEARCH_AGAIN, state=Search.after_not_found)
     dp.register_message_handler(handle_invalid_not_found_option, state=Search.after_not_found)
